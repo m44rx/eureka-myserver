8ce63a7b3f1a4a63a096cc8a585ebf479b402635
marx <salimn78@gmail.com>
2022-02-03 22:52:20 -0500

    configuracion completa

---
+++ src/test/java/com/eurek/eurekamyserver/EurekaMyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurek.eurekamyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
